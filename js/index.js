6cfabd242c34895c65cc288e70edbabdf38b6fe0
Senne Scheeprs <senne.scheepers@gmail.com>
2020-04-06 09:53:43 +0200

    responsive design index

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 function handleLoad() {
+    let dropdownButton = document.getElementById("dropdownButton");
+    dropdownButton.addEventListener("click", handleNavbar);
+}
 
+function handleNavbar() {
+    let x = document.getElementById("nav-bar");
+    if (x.className === "nav-bar") {
+        console.log("huts");
+        x.className += " unfolded";
+    } else {
+        console.log("weg");
+        x.className = "nav-bar";
+    }
 }
 
 window.addEventListener("load", handleLoad);
