94a4df0e3a7acaeeff6f2293559a8df7405f0547
Senne Scheeprs <senne.scheepers@gmail.com>
2020-07-23 22:39:03 +0200

    deploy

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function handleLoad() {
     let dropdownButton = document.getElementById("dropdownButton");
     dropdownButton.addEventListener("click", handleNavbar);
+    handleAnimations();
 }
 
 function handleNavbar() {
@@ -12,6 +13,18 @@ function handleNavbar() {
     } else {
         nav.className = "nav-bar";
         buttonIcon.className = "fa fa-bars";
+    }
+}
+
+function handleAnimations() {
+    let elements = document.querySelectorAll(".top");
+    console.log(elements);
+    for (let e of elements) {
+        e.classList.add("slideInTop");
+    }
+    elements = document.querySelectorAll(".right");
+    for (let e of elements) {
+        e.classList.add("slideInRight");
     }
 }
 
